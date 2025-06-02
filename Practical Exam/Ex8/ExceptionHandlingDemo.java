241ebc0303673969839d4c99cb44f94fbd67666a
Mr. Programmer <adarsh.pardeshi22@gmail.com>
2025-06-02 16:41:40 +0530

    2
    2

--- Practical Exam/Ex8/ExceptionHandlingDemo.java
+++ Practical Exam/Ex8/ExceptionHandlingDemo.java
@@ -1,30 +1,30 @@
 // User-defined exception class
 class CustomException extends Exception {
-public CustomException(String message) {
-    super(message);
-}
+    public CustomException(String message) {
+        super(message);
+    }
 }
 public class ExceptionHandlingDemo {
-public static void main(String[] args) {
-// Predefined exception example
-try {
-int result = 10 / 0; // This will cause ArithmeticException
-} catch (ArithmeticException e) {
-System.out.println("Caught predefined exception: " + e);
-}
-// User-defined exception example
-try {
-validateAge(15);
-} catch (CustomException e) {
-System.out.println("Caught user-defined exception: " +
-e.getMessage());
-}
+    public static void main(String[] args) {
+    // Predefined exception example
+    try {
+        int result = 10 / 0; // This will cause ArithmeticException
+    } catch (ArithmeticException e) {
+        System.out.println("Caught predefined exception: " + e);
+    }
+    // User-defined exception example
+    try {
+        validateAge(15);
+    } catch (CustomException e) {
+        System.out.println("Caught user-defined exception: " +
+        e.getMessage());
+    }
 }
 static void validateAge(int age) throws CustomException {
-if (age < 18) {
-throw new CustomException("Age must be 18 or above.");
-} else {
-System.out.println("Valid age.");
-}
-}
+    if (age < 18) {
+        throw new CustomException("Age must be 18 or above.");
+    } else {
+        System.out.println("Valid age.");
+    }
+    }
 }
